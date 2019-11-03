08f8862c0cb2511dc885b381e27ea87c3f44b4c3
Farid Faraji <farid.faraji@alumni.ubc.ca>
2019-11-03 00:19:54 -0700

    Lta 9 test cases login validation (#7)
    These are the test cases for validation of sign in/sign up credentials

--- lib/src/utils/validator.dart
+++ lib/src/utils/validator.dart
@@ -1,5 +1,5 @@
 class Validator {
-  String validateName(String value) {
+  static String validateName(String value) {
     String pattern = r'(^[a-zA-Z ]*$)';
     RegExp regExp = new RegExp(pattern);
     if (value.length == 0) {
@@ -10,20 +10,20 @@ class Validator {
     return null;
   }
 
-  String validateMobile(String value) {
+  static String validateMobile(String value) {
     String pattern = r'(^[0-9]*$)';
     RegExp regExp = new RegExp(pattern);
     if (value.length == 0) {
       return "Mobile is Required";
     } else if (value.length != 10) {
-      return "Mobile number must 10 digits";
+      return "Mobile number must be 10 digits";
     } else if (!regExp.hasMatch(value)) {
       return "Mobile Number must be digits";
     }
     return null;
   }
 
-  String validatePasswordLength(String value) {
+  static String validatePasswordLength(String value) {
     if (value.length == 0) {
       return "Password can't be empty";
     } else if (value.length < 10) {
@@ -32,7 +32,7 @@ class Validator {
     return null;
   }
 
-  String validateEmail(String value) {
+  static String validateEmail(String value) {
     String pattern =
         r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
     RegExp regExp = new RegExp(pattern);
