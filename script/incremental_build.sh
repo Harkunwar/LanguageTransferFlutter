94a84372e964173025828b548546855dde5da845
Farid Faraji <farid.faraji45@gmail.com>
2019-11-03 12:06:05 -0800

    changed

--- script/incremental_build.sh
+++ script/incremental_build.sh
@@ -18,8 +18,8 @@ else
   # We need this check because some CIs can do a single branch clones with a limited history of commits.
   if BRANCH_BASE_SHA=$(git merge-base --fork-point FETCH_HEAD HEAD); then
     echo "Checking changes from $BRANCH_BASE_SHA..."
-    FLUTTER_CHANGED_GLOBAL=`git diff --name-only $BRANCH_BASE_SHA HEAD | grep -v packages | wc -l`
-    FLUTTER_CHANGED_PACKAGES=`git diff --name-only $BRANCH_BASE_SHA HEAD | grep -o "packages/[^/]*" | sed -e "s/packages\///g" | sort | uniq | paste -s -d, -`
+    FLUTTER_CHANGED_GLOBAL=`git diff --name-only $BRANCH_BASE_SHA HEAD | grep -v / | wc -l`
+    FLUTTER_CHANGED_PACKAGES=`git diff --name-only $BRANCH_BASE_SHA HEAD | grep -o "/[^/]*" | sed -e "s/\///g" | sort | uniq | paste -s -d, -`
   else
     echo "Cannot find a merge base for the current branch to run an incremental build..."
     echo "Please rebase your branch onto the latest master!"
