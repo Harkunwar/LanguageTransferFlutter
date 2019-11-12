3c06f544d775546df18f76513cfdf1850d33050f
Farid Faraji <farid.faraji45@gmail.com>
2019-11-11 17:40:02 -0800

    children-removed

--- lib/src/ui/widgets/textformfield.dart
+++ lib/src/ui/widgets/textformfield.dart
@@ -55,29 +55,25 @@ class _CustomTextFieldState extends State<CustomTextField> {
     final double _pixelRatio = MediaQuery.of(context).devicePixelRatio;
     final bool large = ResponsiveWidget.isScreenLarge(_width, _pixelRatio);
     final bool medium = ResponsiveWidget.isScreenMedium(_width, _pixelRatio);
-    return Column(
-      children: <Widget>[
-        Material(
-          borderRadius: BorderRadius.circular(30.0),
-          elevation: large ? 12 : (medium ? 10 : 8),
-          child: TextFormField(
-            controller: textEditingController,
-            keyboardType: keyboardType,
-            obscureText: obscureText,
-            cursorColor: Colors.blue[200],
-            onChanged: (text) => setErrorMessage(text),
-            decoration: InputDecoration(
-              prefixIcon: Icon(icon, color: Colors.deepPurple, size: 20),
-              suffixIcon: valid ? null : Icon(Icons.cancel, color: Colors.red[600], size :20),
-              hintText: hint,
-              border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(30.0),
-                  borderSide: BorderSide.none
-              ),
-            ),
+    return Material(
+      borderRadius: BorderRadius.circular(30.0),
+      elevation: large ? 12 : (medium ? 10 : 8),
+      child: TextFormField(
+        controller: textEditingController,
+        keyboardType: keyboardType,
+        obscureText: obscureText,
+        cursorColor: Colors.blue[200],
+        onChanged: (text) => setErrorMessage(text),
+        decoration: InputDecoration(
+          prefixIcon: Icon(icon, color: Colors.deepPurple, size: 20),
+          suffixIcon: valid ? null : Icon(Icons.cancel, color: Colors.red[600], size :20),
+          hintText: hint,
+          border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30.0),
+              borderSide: BorderSide.none
           ),
         ),
-      ],
+      ),
     );
   }
 
